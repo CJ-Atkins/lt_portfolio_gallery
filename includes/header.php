741b5eee2aa483373004bf1202be7a19301137dd
CJ-Atkins <connoratkins@hotmail.co.uk>
2023-02-01 19:36:33 +0000

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="/css/styles.css">
+   <title>Leslie Tran Character Design & Illustration</title>
+</head>
+
+<body>
+
+   <div class="container">
+      <header>
+         <h1>Leslie Tran</h1>
+         <h3>Character Design ∙ Illustration ∙ Visual Development</h3>
+      </header>
+      <nav>
+         <ul>
+            <li>Character Design</li>
+            <li>Illustration</li>
+            <li>Resume</li>
+         </ul>
+      </nav>
+
+   </div>
+</body>
+
+</html>
